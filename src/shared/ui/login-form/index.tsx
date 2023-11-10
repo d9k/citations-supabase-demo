6af40336f4ef76eb34848d1c629ea2fec2c24eb7
d9k <d9k@ya.ru>
2023-11-10 19:20:36 +0500

    LoginForm basic styling

--- src/shared/ui/login-form/index.tsx
+++ src/shared/ui/login-form/index.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { useToggle, upperFirst } from '@mantine/hooks';
 import { useForm } from '@mantine/form';
 import {
@@ -12,50 +13,74 @@ import {
   Checkbox,
   Anchor,
   Stack,
+  Space,
 } from '@mantine/core';
+
+export type LoginFormValues = {
+  email: string;
+  name: string;
+  login: string;
+  password: string;
+  terms: boolean;
+}
 
 export type LoginFormProps = PaperProps & {
   demoField?: boolean;
+  initialValues?: Partial<LoginFormValues>;
+  projectName: string;
 }
+
+export const defaultLoginFormValues = (): Partial<LoginFormValues> => ({
+  terms: true,
+})
+
+const fieldsValidateFuncrions = {
+  email: (val?: string) => (/^\S+@\S+$/.test(val || '') ? null : 'Invalid email'),
+  password: (val?: string) => ((val || '').length <= 6 ? 'Password should include at least 6 characters' : null),
+}
+
 
 /**
  * forked from
  * https://github.com/mantinedev/ui.mantine.dev/blob/6f9c568ee161ab3239b826af92dd48415e319cf8/lib/AuthenticationForm/AuthenticationForm.tsx
  */
-export function LoginForm(props: LoginFormProps) {
+export function LoginForm({projectName, initialValues = {}, ...props}: LoginFormProps) {
   const [type, toggle] = useToggle(['login', 'register']);
-  const form = useForm({
-    initialValues: {
-      email: '',
-      name: '',
-      password: '',
-      terms: true,
-    },
 
-    validate: {
-      email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
-      password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
-    },
+  const mergedInitialValues: Partial<LoginFormValues> = useMemo(() => (
+    {...defaultLoginFormValues(), ...initialValues}
+  ), [initialValues]);
+
+  const form = useForm({
+    initialValues: mergedInitialValues,
+    validate: fieldsValidateFuncrions,
   });
+
+  const isRegister = type === 'register';
+
+  const inputSize = "lg";
 
   return (
     <Paper radius="md" p="xl" withBorder {...props}>
       <Text size="lg" fw={500}>
-        Welcome to Mantine, {type} with
+        { isRegister ? `Register in ${projectName}` : `Login to ${projectName}` }
       </Text>
 
-      <Group grow mb="md" mt="md">
+      {/* <Group grow mb="md" mt="md">
         <Button size="xs" radius="md" variant="default">Google</Button>
         <Button size="xs" radius="md" variant="default">Twitter</Button>
-      </Group>
+      </Group> */}
 
-      <Divider label="Or continue with email" labelPosition="center" my="lg" />
+      <Space h="md" />
+
+      {/* <Divider label="Or continue with email" labelPosition="center" my="lg" /> */}
 
       <form onSubmit={form.onSubmit(() => {})}>
         <Stack>
-          {type === 'register' && (
+          {isRegister && (
             <TextInput
-              label="Name"
+              required
+              size={inputSize}
               placeholder="Your name"
               value={form.values.name}
               onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
@@ -63,10 +88,21 @@ export function LoginForm(props: LoginFormProps) {
             />
           )}
 
+          {isRegister && (
+            <TextInput
+              required
+              size={inputSize}
+              placeholder="Your login"
+              value={form.values.login}
+              onChange={(event) => form.setFieldValue('login', event.currentTarget.value)}
+              radius="md"
+            />
+          )}
+
           <TextInput
             required
-            label="Email"
-            placeholder="hello@mantine.dev"
+            size={inputSize}
+            placeholder={isRegister ? "Email" : "Email of login"}
             value={form.values.email}
             onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
             error={form.errors.email && 'Invalid email'}
@@ -75,13 +111,15 @@ export function LoginForm(props: LoginFormProps) {
 
           <PasswordInput
             required
-            label="Password"
+            size={inputSize}
             placeholder="Your password"
             value={form.values.password}
             onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
             error={form.errors.password && 'Password should include at least 6 characters'}
             radius="md"
           />
+
+          <Space h="xs" />
 
           {type === 'register' && (
             <Checkbox
@@ -91,6 +129,7 @@ export function LoginForm(props: LoginFormProps) {
             />
           )}
         </Stack>
+
 
         <Group align="flex-end" justify="space-between" mt="xl">
           <Anchor
