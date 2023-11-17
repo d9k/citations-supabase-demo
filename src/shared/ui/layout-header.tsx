7242e4f732626b7a729b57f38b3debaa7b5e50fe
d9k <d9k@ya.ru>
2023-11-17 18:44:31 +0500

    refactor(LayoutHeader): move some styles to fela

--- src/shared/ui/layout-header.tsx
+++ src/shared/ui/layout-header.tsx
@@ -7,6 +7,9 @@ import { ReactNode } from 'react';
 // , NavLink
 import { matchPath } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import { useMantineDarkMode } from '/~/shared/lib/mantine/useMantineDarkMode.ts';
+import { useFela } from 'react-fela';
+import { cssProps } from '/~/shared/react/cssProps.ts';
 
 export type MenuItem = {
   active?: boolean;
@@ -29,6 +32,10 @@ export function LayoutHeader({
   menuItems,
   menuGap = 0,
 }: HeaderProps) {
+  const { css } = useFela();
+  const dark = useMantineDarkMode();
+
+  // console.log('__TEST__:', m);
   // const [opened, { toggle }] = useDisclosure(false);
   // const [active, setActive] = useState(menuItems[0].link);
 
@@ -50,7 +57,18 @@ export function LayoutHeader({
   });
 
   return (
-    <header className="header">
+    <header className={`header ${css(cssProps({
+      marginBottom: 120,
+      backgroundColor: 'var(--mantine-color-body)',
+      borderBottom: `1px solid var(${
+          dark ? '--mantine-color-dark-4'
+               : '--mantine-color-gray-3'
+      })`,
+      fontSize: '--mantine-font-size-md',
+      display: 'flex',
+      flexDirection: 'row-reverse',
+      justifyContent: 'space-between',
+    }))}`}>
       <Group gap={0} className="header_menu">
         {menuNodes}
       </Group>
