875b146a0957fec032ede8a3027a6b6faaa97cc0
d9k <d9k@ya.ru>
2023-11-19 08:16:29 +0500

    storybook(LoginForm): split to *.tdd.stories.tsx with interactive TDD tests

--- src/features/auth/login-form/LoginForm.stories.tsx
+++ src/features/auth/login-form/LoginForm.stories.tsx
@@ -6,50 +6,27 @@ import { expect } from '@storybook/jest';
 import { LoginForm } from './index.tsx';
 import { waitForMantineStylesLoaded } from '/~/shared/lib/storybook/waitForMantineStylesLoaded.ts';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'features/Auth/LoginForm',
   component: LoginForm,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    // layout: 'centered',
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    // backgroundColor: { control: 'color' },
-  },
 } satisfies Meta<typeof LoginForm>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
   args: {
     projectName: 'Secret Project',
   },
 };
 
-export const TestLoginRequired: Story = {
+export const Secondary: Story = {
   args: {
-    ...Primary.args,
+    projectName: 'Another Secret Project',
   },
-  async play(params) {
-    const { canvasElement } = params;
-
-    await waitForMantineStylesLoaded(canvasElement);
-
-    const canvas = within(canvasElement);
-
-    const inputEmailOrLogin = canvas.getByPlaceholderText('Email or login');
-    const inputPassword = canvas.getByPlaceholderText('Your password');
-
-    expect(inputEmailOrLogin).toHaveAttribute('required');
-    expect(inputPassword).toHaveAttribute('required');
-    await userEvent.type(inputEmailOrLogin, 'shrek@is.life');
-    await userEvent.type(inputPassword, 'PrFiona');
-  },
+  tags: [],
 };
---
+++ src/features/auth/login-form/LoginForm.tdd.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import meta, { Primary } from './LoginForm.stories.tsx';
+import { userEvent, waitFor, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { waitForMantineStylesLoaded } from '/~/shared/lib/storybook/waitForMantineStylesLoaded.ts';
+import { LoginForm } from './index.tsx';
+
+const tddMeta = {
+  ...meta,
+  title: 'features/Auth/LoginForm',
+  tags: [],
+} satisfies Meta<typeof LoginForm>;
+
+export default tddMeta;
+
+type Story = StoryObj<typeof tddMeta>;
+
+export const TestLoginRequired: Story = {
+  args: {
+    ...Primary.args,
+  },
+  async play(params: any) {
+    const { canvasElement } = params;
+
+    await waitForMantineStylesLoaded(canvasElement);
+
+    const canvas = within(canvasElement);
+
+    const inputEmailOrLogin = canvas.getByPlaceholderText('Email or login');
+    const inputPassword = canvas.getByPlaceholderText('Your password');
+
+    expect(inputEmailOrLogin).toHaveAttribute('required');
+    expect(inputPassword).toHaveAttribute('required');
+    await userEvent.type(inputEmailOrLogin, 'shrek@is.life');
+    await userEvent.type(inputPassword, 'PrFiona');
+  },
+  // docs
+};
