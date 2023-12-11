ce0f623220c53c9fee0e942590edcb17730e0491
d9k <d9k@ya.ru>
2023-12-12 03:44:26 +0500

    PageFrameLayout: use AppShell

--- src/pages/layouts/PageFrameLayout.tsx
+++ src/pages/layouts/PageFrameLayout.tsx
@@ -3,43 +3,66 @@ import { WithChildren } from '/~/shared/lib/react/WithChildren.ts';
 import { LayoutHeader } from '/~/shared/ui/layout-header.tsx';
 import { useSupabaseUser } from '/~/shared/providers/supabase/user.ts';
 
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
 export const PageFrameLayout = ({ children }: WithChildren) => {
   const supabaseUser = useSupabaseUser();
   const email = supabaseUser?.email;
   const userName = email?.split('@')[0];
+  const [opened, { toggle }] = useDisclosure();
 
   return (
-    <>
-      <LayoutHeader
-        menuItems={[
-          {
-            path: '/',
-            caption: 'Home',
-          },
-          {
-            path: '/demo',
-            caption: 'Demo',
-          },
-          ...(supabaseUser
-            ? [{
-              path: '/profile',
-              caption: `${userName} profile`,
-            }, {
-              path: '/logout',
-              caption: 'Log out',
-            }]
-            : [
-              {
-                path: '/login',
-                caption: 'Login',
-              },
-            ]),
-        ]}
-      />
+    <AppShell
+      header={{ height: 30 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { desktop: !opened, mobile: !opened },
+      }}
+      padding='md'
+    >
+      <AppShell.Header>
+        <LayoutHeader
+          menuItems={[
+            {
+              path: '/',
+              caption: 'Home',
+            },
+            {
+              path: '/demo',
+              caption: 'Demo',
+            },
+            ...(supabaseUser
+              ? [{
+                path: '/profile',
+                caption: `${userName} profile`,
+              }, {
+                path: '/logout',
+                caption: 'Log out',
+              }]
+              : [
+                {
+                  path: '/login',
+                  caption: 'Login',
+                },
+              ]),
+          ]}
+        >
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            size='md'
+          />
+          <div>Logo</div>
+        </LayoutHeader>
+      </AppShell.Header>
 
-      <main>
+      <AppShell.Navbar p='md'>Navbar</AppShell.Navbar>
+
+      <AppShell.Main>
         {children}
-      </main>
-    </>
+      </AppShell.Main>
+    </AppShell>
   );
 };
