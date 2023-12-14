b2072da87c6a6eef85e03ec4fcbae4181fc20785
d9k <d9k@ya.ru>
2023-12-14 17:20:45 +0500

    TablesList: show links

--- src/features/tables/list.tsx
+++ src/features/tables/list.tsx
@@ -2,11 +2,15 @@ import { DbSchema } from '/~/shared/api/supabase/json-schema.types.ts';
 import schemaData from '/~/shared/api/supabase/schema.generated.json' assert {
   type: 'json',
 };
-
-// const schema = schemaData as DbSchema;
+const schema = schemaData as unknown as DbSchema;
 
 export const TablesList = () => {
-  console.log('TablesList:', schemaData);
+  console.log('TablesList:', schema);
+  const publicSchema = schema['public'];
 
-  return <></>;
+  const list = Object.entries(publicSchema).map(([tableName, _tableSchema]) => {
+    return <a key={tableName} href={`table/${tableName}`}>{tableName}</a>;
+  });
+
+  return <>{list}</>;
 };
