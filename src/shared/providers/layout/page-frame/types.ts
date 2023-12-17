13e57749f4bbaa758ad154e040c7fd15510fc040
d9k <d9k@ya.ru>
2023-12-18 01:56:19 +0500

    * also added PageFrameLayoutProviderContextUpdate.forceRerender

--- src/shared/providers/layout/page-frame/types.ts
+++ src/shared/providers/layout/page-frame/types.ts
@@ -1,5 +1,3 @@
 import React from 'react';
 
 export type PageFrameComponentType = React.ElementType;
-/** We can't store function in useState */
-export type PageFrameComponentWrappedType = () => React.ElementType;
