b0785abcb6d7dd5585fe5e5cb670ab81ed4e68e1
d9k <d9k@ya.ru>
2023-12-18 07:03:56 +0500

    story(DataGrid): fix layout, columns freeze, types, +stories LessRows, NoRows

--- src/shared/ui/data-grid/data-grid.stories.tsx
+++ src/shared/ui/data-grid/data-grid.stories.tsx
@@ -4,53 +4,91 @@ import demoDataBoredApiTasksRaw from '/~/shared/demo-data/tasks-from-bored-api.g
 
 import { BoredApiTask } from '/~/shared/demo-data/bored-api-task.ts';
 
-import { Column, DataGrid } from './index.tsx';
+import { Column, DataGrid, DataGridProps } from './index.tsx';
+
+import { take } from 'lodash';
 
 const meta = {
   title: 'Shared/DataGrid',
-  component: DataGrid,
+  component: DataGrid<BoredApiTask>,
   parameters: {
-    layout: 'centered',
+    layout: 'fullscreen',
   },
   argTypes: {},
-} satisfies Meta<typeof DataGrid>;
+  // decorators: [
+  //   (Story) => (
+  //     <div
+  //       style={{
+  //         display: 'grid',
+  //         gridTemplateColumns: 'auto 1fr',
+  //       }}
+  //     >
+  //       <div
+  //         style={{
+  //           display: 'flex',
+  //           flexDirection: 'column',
+  //           boxSizing: 'border-box',
+  //           blockSize: '100vh',
+  //           padding: '8px',
+  //           contain: 'inline-size',
+  //         }}
+  //       >
+  //         <Story />
+  //       </div>
+  //     </div>
+  //   ),
+  // ],
+} satisfies Meta<typeof DataGrid<BoredApiTask>>;
 
 const demoDataBoredApiTasks =
   demoDataBoredApiTasksRaw as unknown as BoredApiTask[];
 
 console.log('__TEST__: demoDataBoredApiTasks:', demoDataBoredApiTasks);
 
-const columns: Column<BoredApiTask>[] = [
+const TEXT_FIELD_WIDTH = 400;
+const TYPE_WIDTH = 100;
+const SMALL_NUMBER_WIDTH = 100;
+
+const columnsRaw: Column<BoredApiTask>[] = [
   {
     key: 'key',
     name: 'key',
+    width: 80,
     frozen: true,
   },
   {
     key: 'activity',
+    width: TEXT_FIELD_WIDTH,
     name: 'activity',
   },
   {
     key: 'link',
+    width: TEXT_FIELD_WIDTH,
     name: 'link',
   },
   {
     key: 'type',
+    width: TYPE_WIDTH,
     name: 'type',
   },
   {
     key: 'participants',
     name: 'participants',
+    width: SMALL_NUMBER_WIDTH,
   },
   {
     key: 'accessibility',
     name: 'accessibility',
+    width: SMALL_NUMBER_WIDTH,
   },
   {
     key: 'price',
     name: 'price',
+    width: SMALL_NUMBER_WIDTH,
   },
 ];
+
+const columns = columnsRaw.map((c) => ({ ...c, resizable: true }));
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -59,5 +97,20 @@ export const Primary: Story = {
   args: {
     columns,
     rows: demoDataBoredApiTasks,
+    rowKeyGetter: (row) => row.key,
+  },
+};
+
+export const LessRows: Story = {
+  args: {
+    ...Primary.args,
+    rows: take(demoDataBoredApiTasks, 5),
+  },
+};
+
+export const NoRows: Story = {
+  args: {
+    ...Primary.args,
+    rows: [],
   },
 };
--- src/shared/ui/data-grid/index.tsx
+++ src/shared/ui/data-grid/index.tsx
@@ -8,7 +8,6 @@ import VendorDataGrid, {
 import { useFela } from '/~/deps/react-fela/index.ts';
 import { cssProps } from '/~/shared/lib/react/cssProps.ts';
 import { EmptyRowsRenderer } from './empty-rows.tsx';
-import { useCallback } from 'react';
 
 type Key = string | number;
 
@@ -20,10 +19,6 @@ export type Column<TRow, TSummaryRow = unknown> = VendorColumn<
 export type DataGridProps<R, SR = unknown, K extends Key = Key> =
   VendorDataGridProps<R, SR, K>;
 
-const handleRowsChange = (a: any) => {
-  console.log(a);
-};
-
 // function rowKeyGetter(row: any) {
 //   return row.id;
 // }
@@ -34,20 +29,25 @@ export const DataGrid = <R, SR = unknown, K extends Key = Key>(
   const { css } = useFela();
 
   // TODO add idKey props
-  // deno-lint-ignore no-explicit-any
-  const { rowKeyGetter = (row) => (row as any).id } = props;
+  const {
+    // deno-lint-ignore no-explicit-any
+    rowKeyGetter = (row) => (row as any).id,
+    className,
+  } = props;
+
+  const felaCssClass = css({
+    '--rdg-font-size': '14px',
+    ...cssProps({
+      blockSize: '100%',
+      fontFamily: 'var(--mantine-font-family)',
+      maxHeight: '100vh',
+    }),
+  });
 
   return (
     <VendorDataGrid
       {...props}
-      className={css({
-        '--rdg-font-size': '14px',
-        ...cssProps({
-          blockSize: '100%',
-          fontFamily: 'var(--mantine-font-family)',
-        }),
-      })}
-      onRowsChange={handleRowsChange}
+      className={`${felaCssClass} ${className}`}
       rowKeyGetter={rowKeyGetter}
       renderers={{ noRowsFallback: <EmptyRowsRenderer key='no-rows' /> }}
     />
