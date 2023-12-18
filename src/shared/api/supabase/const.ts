7149ae6b10d8aca52c69782cf5821177576cc109
d9k <d9k@ya.ru>
2023-12-18 07:10:15 +0500

    SupabaseAutogeneratedDataGrid: frozen columns

--- src/shared/api/supabase/const.ts
+++ src/shared/api/supabase/const.ts
@@ -1,2 +1,4 @@
 export const COOKIE_NAME_SUPABASE_ACCESS_TOKEN = 'my-supabase-access-token';
 export const COOKIE_NAME_SUPABASE_REFRESH_TOKEN = 'my-supabase-refresh-token';
+
+export const COLUMNS_TO_FREEZE = ['id'];
