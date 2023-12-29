0fdf40e5aebbd8b24852a1e99a644335c2e1838d
d9k <d9k@ya.ru>
2023-12-29 08:01:24 +0500

    Command "build:with-relative-imports-transform-and-run"

---
+++ script/build-with-relative-imports-transform-and-run.ts
@@ -0,0 +1,32 @@
+import * as dotenv from 'dotenv';
+
+import { envValueRequire } from '/~/shared/lib/deno/env.ts';
+
+import { runCommandWithPipedOutput } from '/~/shared/lib/deno/run.ts';
+import * as path from 'std/path';
+
+await dotenv.load({ export: true });
+
+const PROJECT_COPY_WITH_IMPORTS_TRANSFORMED_TO_RELATIVE_PATH = envValueRequire(
+  'PROJECT_COPY_WITH_IMPORTS_TRANSFORMED_TO_RELATIVE_PATH',
+);
+
+const buildOutput = path.join(
+  PROJECT_COPY_WITH_IMPORTS_TRANSFORMED_TO_RELATIVE_PATH,
+  '.ultra',
+);
+
+await runCommandWithPipedOutput('deno', [
+  'task',
+  'prebuild:copy-and-transform-imports-to-relative',
+]);
+
+await runCommandWithPipedOutput('deno', [
+  'task',
+  'build',
+], { cwd: PROJECT_COPY_WITH_IMPORTS_TRANSFORMED_TO_RELATIVE_PATH });
+
+await runCommandWithPipedOutput('deno', [
+  'task',
+  'start',
+], { cwd: buildOutput });
